e9cbb6b5cbc958a300473f1cb0245883d0e90dff
TrevisanGMW <TrevisanGMW@gmail.com>
2020-11-14 20:25:42 -0800

    Added instructions

--- miscellaneous/load_bullet.py
+++ miscellaneous/load_bullet.py
@@ -1,18 +1,18 @@
 """
- Load Bifrost Script - This script will try to load the plugin Bifrost in case it's not already loaded.
+ Load Bullet Script - This script will try to load the plugin Bullet in case it's not already loaded.
  This is used to make Maya open faster (with "Auto Load" checked off in the Plug-in Manager)
  
  How to use it:
- 1. Go to your Plug-in Manager and uncheck "Auto Load" for the plugins listed below (under plugins_to_load)
+ 1. Go to your Plug-in Manager and uncheck "Auto Load" for the plugin bullet.mll and AbcBullet.mel
  2. Create a shelf button containing the code in this script
- 3. If you ever need to use Bifrost, just click on the shelf button
+ 3. If you ever need to use MASH, just click on the shelf button
  
  @Guilherme Trevisan - 2020-11-14 - github.com/TrevisanGMW
 """
 import sys
 
-# Load
-plugins_to_load = ['bifmeshio', 'bifrostGraph', 'bifrostshellnode', 'bifrostvisplugin', 'Boss', 'mayaVnnPlugin']
+# Load Bullet
+plugins_to_load = ['AbcBullet', 'bullet']
 feedback = ''
 
 for plugin in plugins_to_load:
--- miscellaneous/load_xgen.py
+++ miscellaneous/load_xgen.py
@@ -1,6 +1,12 @@
 """
  Load xGen Script - This script will try to load the plugin xGen in case it's not already loaded.
- This is used to make Maya open faster (with "auto load" off for the plugin)
+ This is used to make Maya open faster (with "Auto Load" checked off in the Plug-in Manager)
+ 
+ How to use it:
+ 1. Go to your Plug-in Manager and uncheck "Auto Load" for the plugin xgenToolkit.mll
+ 2. Create a shelf button containing the code in this script
+ 3. If you ever need to use xGen, just click on the shelf button
+ 
  @Guilherme Trevisan - 2020-11-14 - github.com/TrevisanGMW
 """
 import sys
@@ -10,6 +16,8 @@ plugin_name = 'xgenToolkit'
 if not cmds.pluginInfo(plugin_name, q=True, loaded=True):
     try:
         cmds.loadPlugin(plugin_name)
+        if cmds.pluginInfo(plugin_name, q=True, loaded=True):
+                sys.stdout.write(plugin_name + ' is now loaded.')
     except:
         message = '<span style=\"color:#FF0000;text-decoration:underline;\">' +  plugin_name + '</span> doesn\'t seem to be installed.'
         cmds.inViewMessage(amg=message, pos='botLeft', fade=True, alpha=.9)
