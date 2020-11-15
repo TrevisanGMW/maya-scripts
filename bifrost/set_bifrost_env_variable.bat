e064d13946c50594946086e1a7b495f1c3397fe8
TrevisanGMW <TrevisanGMW@gmail.com>
2020-11-14 17:42:32 -0800

    Initial upload

---
+++ bifrost/set_bifrost_env_variable.bat
@@ -0,0 +1,54 @@
+@echo off
+@title =  Create Environment Variable for Bifrost
+SET BifrostVar=%~dp0bifrost_lib_config.json
+:MENU
+cls
+color 0A
+@echo.
+@echo.
+@echo.       This script auto creates the BIFROST_LIB_CONFIG_FILES environment variable for Bifrost.
+@echo.       Put the ".bat" file in the folder where your "bifrost_lib_config.json" is and run it.
+@echo.
+@echo.
+@echo. 	  1 = Overwrite Current Bifrost Variable (1st Var)
+@echo. 	  2 = Add to Current Bifrost Variable (2nd, 3rd, etc..)
+@echo. 	  3 = Open System Properties
+@echo. 	  4 = Print BIFROST_LIB_CONFIG_FILES variable
+@echo.
+@echo.
+@echo off
+SET /P M=Type 1, 2, 3 or 4 then press ENTER:
+IF %M%==1 GOTO OVERWRITE
+IF %M%==2 GOTO ADD
+IF %M%==3 GOTO SYSDM
+IF %M%==4 GOTO PRINT
+GOTO EOF
+
+:OVERWRITE
+@echo on
+SETX BIFROST_LIB_CONFIG_FILES "%BifrostVar%"
+@echo off
+pause
+GOTO MENU
+
+:ADD
+SET NewVar=%BIFROST_LIB_CONFIG_FILES%;%BifrostVar%
+SETX BIFROST_LIB_CONFIG_FILES "%NewVar%"
+pause
+GOTO MENU
+
+:PRINT
+@echo.
+@echo.
+@echo.   %BIFROST_LIB_CONFIG_FILES%
+@echo.
+@echo.
+pause
+GOTO MENU
+
+:SYSDM
+sysdm.cpl
+GOTO MENU
+
+:EOF
+exit
