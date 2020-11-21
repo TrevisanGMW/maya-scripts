273e196f1634e6299b003491decb869d06a0fb60
TrevisanGMW <TrevisanGMW@gmail.com>
2020-11-20 18:44:14 -0800

    Added better feedback

--- miscellaneous/load_mash.py
+++ miscellaneous/load_mash.py
@@ -6,21 +6,76 @@
  1. Go to your Plug-in Manager and uncheck "Auto Load" for the plugin MASH.mll
  2. Create a shelf button containing the code in this script
  3. If you ever need to use MASH, just click on the shelf button
- 
+  
  @Guilherme Trevisan - 2020-11-14 - github.com/TrevisanGMW
+ 
+ 1.1 - 2020-11-20
+ Updated with a function and better feedback
 """
+import maya.cmds as cmds
 import sys
 
-# Load Mash
-plugin_name = 'MASH'
-if not cmds.pluginInfo(plugin_name, q=True, loaded=True):
-    try:
-        cmds.loadPlugin(plugin_name)
-        if cmds.pluginInfo(plugin_name, q=True, loaded=True):
-                sys.stdout.write(plugin_name + ' is now loaded.')
-    except:
-        message = '<span style=\"color:#FF0000;text-decoration:underline;\">' +  plugin_name + '</span> doesn\'t seem to be installed.'
-        cmds.inViewMessage(amg=message, pos='botLeft', fade=True, alpha=.9)
-else:
-    sys.stdout.write(plugin_name + ' is already loaded')
+plugins_to_load = ['MASH']
+
+def gtu_load_plugins(plugin_list):
+    ''' 
+    Attempts to load provided plug-ins, then gives the user feedback about their current state. (Feedback through inView messages and stdout.write messages)
     
+            Parameters:
+                plugin_list (list): A list of strings containing the name of the plug-ings yo uwant to load
+    
+    '''
+    already_loaded = []
+    not_installed = []
+    now_loaded = []
+    
+    # Load Plug-in
+    for plugin in plugin_list:
+        if not cmds.pluginInfo(plugin, q=True, loaded=True):
+            try:
+                cmds.loadPlugin(plugin)
+                if cmds.pluginInfo(plugin, q=True, loaded=True):
+                    now_loaded.append(plugin)
+            except:
+                not_installed.append(plugin)
+        else:
+            already_loaded.append(plugin)
+    
+    # Give Feedback
+    if len(not_installed) > 0:
+        message_feedback = ''
+        for str in not_installed:
+            message_feedback += str + ', '
+        is_plural = 'plug-ins don\'t'
+        if len(not_installed) == 1:
+            is_plural = 'plug-in doesn\'t'
+        message = '<span style=\"color:#FF0000;text-decoration:underline;\">' +  message_feedback[:-2] + '</span> ' + is_plural + ' seem to be installed.'
+        cmds.inViewMessage(amg=message, pos='botLeft', fade=True, alpha=.9)
+        sys.stdout.write(message_feedback[:-2] + ' ' + is_plural + ' seem to be installed.')
+        
+    if len(now_loaded) > 0:
+        message_feedback = ''
+        for str in now_loaded:
+            message_feedback += str + ', '
+        is_plural = 'plug-ins are'
+        if len(now_loaded) == 1:
+            is_plural = 'plug-in is'
+        message = '<span style=\"color:#FF0000;text-decoration:underline;\">' +  message_feedback[:-2] + '</span> ' + is_plural + ' now loaded.'
+        cmds.inViewMessage(amg=message, pos='botLeft', fade=True, alpha=.9)
+        sys.stdout.write(message_feedback[:-2] + ' ' + is_plural + ' now loaded.')
+    
+    if len(already_loaded) > 0:
+        message_feedback = ''
+        for str in already_loaded:
+            message_feedback += str + ', '
+        is_plural = 'plug-ins are'
+        if len(already_loaded) == 1:
+            is_plural = 'plug-in is'
+        message = '<span style=\"color:#FF0000;\">' +  message_feedback[:-2] + '</span> ' + is_plural +  ' already loaded.'
+        cmds.inViewMessage(amg=message, pos='botLeft', fade=True, alpha=.9)
+        sys.stdout.write(message_feedback[:-2] + ' ' + is_plural + ' already loaded.')
+
+
+# Run Script
+if __name__ == '__main__':
+    gtu_load_plugins(plugins_to_load)
